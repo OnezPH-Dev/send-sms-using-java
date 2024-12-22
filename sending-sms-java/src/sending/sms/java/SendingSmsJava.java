09210f191c6f4a0177e030e10b1ecff85155161f
Wilzen Carl R. Tolentino <157259745+OnezPH-Dev@users.noreply.github.com>
2024-12-22 15:34:13 +0845

    Add files via upload

---
+++ sending-sms-java/src/sending/sms/java/SendingSmsJava.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sending.sms.java;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author OnezPH
+ */
+public class SendingSmsJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
